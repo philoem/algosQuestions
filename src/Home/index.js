7b5b36c2a2d377a4792665d15a5c640ac0436a17
Philoem <philoem24@gmail.com>
2023-05-01 17:36:07 +0200

    style: add new module css with text

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../logo.svg'
 import styles from '../Styles/Button.module.css'
+import stylesText from '../Styles/Text.module.css'
 
 function Home() {
 	const navigate = useNavigate()
@@ -10,7 +11,7 @@ function Home() {
 			<img src={logo} className="App-logo" alt="logo" />
 			<p>Medley de questions techniques:</p>
 			<p>
-				en <code className="text-yellow-300">javascript</code>
+				en <code className={stylesText.textCode}>javascript</code>
 			</p>
 			<button type="button" className={styles.btnGoAlgos} onClick={() => navigate('/questionInJs')}>
 				Go to algos
