2da2dd2bcf36950a84aeceec4bbec6664b2e4178
Philoem <philoem24@gmail.com>
2023-05-12 17:35:18 +0200

    style: change grid of template

--- src/QuestionsInJs/ListOfQuestions/index.js
+++ src/QuestionsInJs/ListOfQuestions/index.js
@@ -10,54 +10,59 @@ function ListOfQuestions() {
 		useRevealSolution()
 	return (
 		<div className={styles.container}>
-			{question?.map(({ id, topic, statement, result1, result2 }, index) => {
-				return (
-					<>
-						<div key={`question_${index}`}>
-							<div className={styles.firstSubContainer}>
-								<p>
-									<strong>Question {index + 1}:</strong> {topic}
-								</p>
-							</div>
-							{statement && (
-								<SyntaxHighlighter language="javascript" style={dracula}>
-									{statement}
-								</SyntaxHighlighter>
-							)}
-						</div>
-						<div>
-							<div className={styles.firstSubContainer}>
-								<p>1ère solution: </p>
-								<SwitchButton onClick={() => toggleButtonFirst(id)} key={`swicthFirst_${id}`} />
-								<p className={styles.labelText}>
-									{isVisibleFirst[id] === true ? 'Hide' : 'Reveals'}
-								</p>
-							</div>
-							{isVisibleFirst[id] === true && (
-								<SyntaxHighlighter language="javascript" style={dracula}>
-									{result1}
-								</SyntaxHighlighter>
-							)}
-						</div>
-						<div>
-							{result2 && (
-								<div className={styles.secondSubContainer}>
-									<p>2ème solution:</p>
-									<SwitchButton onClick={() => toggleButtonSecond(id)} key={`swicthSecond_${id}`} />
-									<p className={styles.labelText}>
-										{isVisibleSecond[id] === true ? 'Hide' : 'Reveals'}
+			<>
+				{question?.map(({ id, topic, statement, result1, result2 }, index) => {
+					return (
+						<div className={styles.section}>
+							<div key={`question_${index}`}>
+								<div className={styles.firstSubContainer}>
+									<p>
+										<strong>Question {index + 1}:</strong> {topic}
 									</p>
 								</div>
-							)}
-							{isVisibleSecond[id] && result2 && (
-								<SyntaxHighlighter language="javascript" style={dracula}>
-									{result2}
-								</SyntaxHighlighter>
-							)}
+								{statement && (
+									<SyntaxHighlighter language="javascript" style={dracula}>
+										{statement}
+									</SyntaxHighlighter>
+								)}
+							</div>
+							<div>
+								<div className={styles.firstSubContainer}>
+									<p>1ère solution: </p>
+									<SwitchButton onClick={() => toggleButtonFirst(id)} key={`swicthFirst_${id}`} />
+									<p className={styles.labelText}>
+										{isVisibleFirst[id] === true ? 'Hide' : 'Reveals'}
+									</p>
+								</div>
+								{isVisibleFirst[id] === true && (
+									<SyntaxHighlighter language="javascript" style={dracula}>
+										{result1}
+									</SyntaxHighlighter>
+								)}
+							</div>
+							<div>
+								{result2 && (
+									<div className={styles.secondSubContainer}>
+										<p>2ème solution:</p>
+										<SwitchButton
+											onClick={() => toggleButtonSecond(id)}
+											key={`swicthSecond_${id}`}
+										/>
+										<p className={styles.labelText}>
+											{isVisibleSecond[id] === true ? 'Hide' : 'Reveals'}
+										</p>
+									</div>
+								)}
+								{isVisibleSecond[id] && result2 && (
+									<SyntaxHighlighter language="javascript" style={dracula}>
+										{result2}
+									</SyntaxHighlighter>
+								)}
+							</div>
 						</div>
-					</>
-				)
-			})}
+					)
+				})}
+			</>
 		</div>
 	)
 }
