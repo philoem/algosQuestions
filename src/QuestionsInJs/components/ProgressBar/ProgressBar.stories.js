7515786ddf49205370c4d15e9507bc420d77e221
Philoem <philoem24@gmail.com>
2023-05-15 17:24:06 +0200

    style: add story to progress bar

---
+++ src/QuestionsInJs/components/ProgressBar/ProgressBar.stories.js
@@ -0,0 +1,10 @@
+import ProgressBar from './index'
+
+export default {
+	component: ProgressBar,
+	title: 'Components/ProgressBar'
+}
+
+const Template = (args) => <ProgressBar {...args} />
+
+export const Default = Template.bind({})
