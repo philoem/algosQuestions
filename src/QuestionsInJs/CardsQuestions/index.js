81046d95e21ddad0379b753c80d611cfb4193696
Philoem <philoem24@gmail.com>
2023-05-15 13:56:51 +0200

    refactor: wording

--- src/QuestionsInJs/CardsQuestions/index.js
+++ src/QuestionsInJs/CardsQuestions/index.js
@@ -31,7 +31,7 @@ const CardsQuestions = () => {
 											}}
 										/>
 										<span className={styles.progressText}>
-											{index + 1}/{questionLength} Challenges
+											{index + 1}/{questionLength} Questions
 										</span>
 									</div>
 									{statement && (
