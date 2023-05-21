84e1f6bda952026a3ce2e40cfda45311b3217269
Philoem <philoem24@gmail.com>
2023-05-21 14:08:08 +0200

    refactor: goback button

--- src/Components/GoBack/index.js
+++ src/Components/GoBack/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './index.module.css'
 
 const GoBack = () => {
 	const navigate = useNavigate()
-	const goTo = () => {
+
+	const goTo = useCallback(() => {
 		navigate('..')
-	}
+	}, [navigate])
+
 	return (
 		<div className={styles.containerBtnGoBack}>
 			<button type='button' className={styles.btnGoBack} onClick={goTo}>
