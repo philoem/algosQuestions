274d40dfc5182a608b6f2179082e237d73773b86
Philoem <philoem24@gmail.com>
2023-05-21 14:14:14 +0200

    refactor: questionsinjs

--- src/QuestionsInJs/index.js
+++ src/QuestionsInJs/index.js
@@ -3,16 +3,14 @@ import ScrollToTop from '../Components/ScrollToTop'
 import styles from './index.module.css'
 import CardsQuestions from './CardsQuestions'
 
-const QuestionsInJs = () => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.containerBtnBack}>
-				<GoBack />
-			</div>
-			<CardsQuestions />
-			<ScrollToTop />
+const QuestionsInJs = () => (
+	<div className={styles.container}>
+		<div className={styles.containerBtnBack}>
+			<GoBack />
 		</div>
-	)
-}
+		<CardsQuestions />
+		<ScrollToTop />
+	</div>
+)
 
 export default QuestionsInJs
