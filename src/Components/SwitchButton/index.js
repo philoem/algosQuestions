4cb5f7c6d650b91a680437465b3f9ede1e25755d
Philoem <philoem24@gmail.com>
2023-05-21 14:10:57 +0200

    refactor: switchbutton

--- src/Components/SwitchButton/index.js
+++ src/Components/SwitchButton/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
+/**
+ * Renders a switch button component.
+ *
+ * @param {function} onClick - function to be called when button is clicked to display solution
+ * @return {JSX.Element} A switch button component
+ */
 const SwitchButton = ({ onClick }) => {
 	return (
 		<div className={styles.containerSwitchBtn}>
